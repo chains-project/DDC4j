0bcc06849788377e9df6f2e528dc76f61eec23a1
Elias Lundell <elilun03@gmail.com>
2025-01-27 14:06:34 +0100

    :wrench: setup ddc script

--- ubuntu/scripts/double-compile.sh
+++ ubuntu/scripts/double-compile.sh
@@ -10,10 +10,14 @@ cd $source_code_folder
 
 # --- STAGE 1 ---
 make clean
+git clean -fd
 /root/scripts/bootstrap.sh $source_code_folder $grand_parent_compiler_folder
 cp -r $source_code_folder/build/linux-x86_64-server-release/images/jdk $output_folder/stage1_$grand_parent_compiler_version
+rm -rf $output_folder/stage1_$grand_parent_compiler_version/demo
 
 # --- STAGE 2 ---
 make clean
+git clean -fd
 /root/scripts/bootstrap.sh $source_code_folder $output_folder/stage1_$grand_parent_compiler_version
 cp -r $source_code_folder/build/linux-x86_64-server-release/images/jdk $output_folder/stage2_$grand_parent_compiler_version
+rm -rf $output_folder/stage1_$grand_parent_compiler_version/demo
