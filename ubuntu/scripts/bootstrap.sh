52da64070a69641f23ffc0065758f6168ef70c3c
Elias Lundell <elilun03@gmail.com>
2025-02-07 14:54:31 +0100

    use build jdk

--- ubuntu/scripts/bootstrap.sh
+++ ubuntu/scripts/bootstrap.sh
@@ -6,5 +6,5 @@ boot_jdk_folder=$2
 cd $source_code_folder
 export SOURCE_DATE_EPOCH=$(git log -1 --format="%at")
 # timedatectl set-timezone UTC # Might not be needed in a docker container
-bash configure --with-version-opt=adhoc --with-boot-jdk=$boot_jdk_folder
+bash configure --with-version-opt=adhoc --with-boot-jdk=$boot_jdk_folder --with-build-jdk=$boot_jdk_folder
 make images
