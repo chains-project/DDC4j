969814646dec82eeeb29f68b95536538fc6fa2c2
Elias Lundell <elilun03@gmail.com>
2025-02-07 14:59:48 +0100

    :wrench: separate full and mini ddc scripts

---
+++ ubuntu/mini_ddc_outside_docker_21.0.5-ga.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+image=$1
+
+function ddc {
+    local version=$1
+    echo "PERFORMING DDC ON 21.0.5-ga WITH $version ($(date))"
+    docker run -t --entrypoint "/bin/bash" --network=host --name ddc_container $image /root/scripts/dc_21.0.5-ga_from_sdkman.sh $version
+    docker cp ddc_container:/root/from_$version .
+    docker rm ddc_container
+    echo "DONE COMPILING AND COPYING $version ($(date))"
+}
+
+# --- JDK 21.0.5 ---
+ddc 21.0.5-tem
+ddc 21.0.5-oracle
+
+# --- JDK 20.0.2 ---
+ddc 20.0.2-tem
+ddc 20.0.2-oracle
