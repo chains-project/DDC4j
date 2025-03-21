b94fb075d888baeb908b7c67d612d9148c648d7d
Elias Lundell <elilun03@gmail.com>
2025-03-21 16:32:28 +0100

    copy into container

--- ubuntu/mini_ddc_outside_docker_21.0.5-ga.sh
+++ ubuntu/mini_ddc_outside_docker_21.0.5-ga.sh
@@ -4,12 +4,16 @@ image=$1
 
 function ddc {
     local version=$1
-    local path=$2
+    local local_path=$2
+    local 
     echo "PERFORMING DDC ON 21.0.5-ga WITH $version ($(date))"
     if [ -d "$path" ]; then
-        docker run -t --entrypoint "/bin/bash" --network=host --name ddc_container $image /root/scripts/dc_21.0.5-ga_from_local.sh $version $path
+        docker create -t --entrypoint "/bin/bash" --network=host --name ddc_container $image /root/scripts/dc_21.0.5-ga_from_local.sh $version /root/local_jdk
+        docker cp $path ddc_container:/root/local_jdk
+        docker start -a ddc_container
     else
-        docker run -t --entrypoint "/bin/bash" --network=host --name ddc_container $image /root/scripts/dc_21.0.5-ga_from_sdkman.sh $version
+        docker create -t --entrypoint "/bin/bash" --network=host --name ddc_container $image /root/scripts/dc_21.0.5-ga_from_sdkman.sh $version
+        docker start -a ddc_container
     fi
 
     docker cp ddc_container:/root/from_$version .
@@ -18,5 +22,6 @@ function ddc {
 }
 
 # --- JDK 21.0.5 ---
+ddc 21.0.5-infected_local /root/21.0.5_infected_jdk
 ddc 21.0.5-tem
 ddc 21.0.5-oracle
