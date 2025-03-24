8410ddbed5dbafb93803da1682bb8847ccfb52dd
rfyuz <eskil.nyberg@gmail.com>
2025-03-24 14:10:22 +0100

    :sparkles: Skeleton for JDK from Jikes bootstrap

---
+++ jdk_from_jikes/scripts/bootstrap_jdk/bootstrap_jdk.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# This script implements the bootstrapping outlined by the Bootstrappable Builds
+# project. It is the first step in the process of building a JDK of version 21
+# which does not depend on any Java compiler written in Java.
+
