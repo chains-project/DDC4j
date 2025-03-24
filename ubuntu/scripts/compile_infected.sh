91ca67e105659a19ddc9e63064d130fb59e7be68
Elias Lundell <elilun03@gmail.com>
2025-03-24 10:15:01 +0100

    add script to compile infected trusting trust

---
+++ ubuntu/scripts/compile_infected.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+yes | sdk install java 21.0.5-tem
+
+cd /root
+git clone https://github.com/LogFlames/jdk21u_trusting_trust.git
+
+cd jdk21u_trusting_trust
+
+/root/scripts/bootstrap.sh /root/jdk21u_trusting_trust/infected_jdk /root/.sdkman/candidates/java/21.0.5-tem
+/root/scripts/bootstrap.sh /root/jdk21u_trusting_trust/clean_jdk /root/jdk21u_trusting_trust/infected_jdk/linux-x86_64-server-release/images/jdk
+
+cp -pr /root/jdk21u_trusting_trust/infected_jdk/build/linux-x86_64-server-release/images/jdk /root/infected_jdk
