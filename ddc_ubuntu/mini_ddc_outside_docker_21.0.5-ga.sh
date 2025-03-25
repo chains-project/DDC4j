ba96f2a965c8a895141983064abb8b424fd46e1b
Elias Lundell <elilun03@gmail.com>
2025-03-25 13:44:06 +0100

    :hammer: keep one more path when extracting tar

--- ddc_ubuntu/mini_ddc_outside_docker_21.0.5-ga.sh
+++ ddc_ubuntu/mini_ddc_outside_docker_21.0.5-ga.sh
@@ -32,7 +32,7 @@ function ddc {
     docker cp ddc_container:/root/from_$version.tar.gz .
     docker rm ddc_container
 
-    tar -xzf from_$version.tar.gz --strip-components=2
+    tar -xzf from_$version.tar.gz --strip-components=1
     rm from_$version.tar.gz
     echo "DONE COMPILING AND COPYING $version ($(date))"
 }
