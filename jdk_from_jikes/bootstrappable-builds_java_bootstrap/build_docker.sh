af4e6a3e1c1696a9115b9d5220f480b4bb45eccc
rfyuz <eskil.nyberg@gmail.com>
2025-04-04 10:47:00 +0200

    :fire: We will use guix instead

--- jdk_from_jikes/bootstrappable-builds_java_bootstrap/build_docker.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if ! docker buildx ls --format="{{.Name}}" | grep -q 'insecure-builder'; then
-    docker buildx create --driver-opt image=moby/buildkit:master \
-        --use --name insecure-builder \
-        --buildkitd-flags '--allow-insecure-entitlement security.insecure'
-fi
-docker buildx use insecure-builder
-docker buildx build --allow security.insecure --progress=plain -t bootstrappable-builds_java-bootstrap .
